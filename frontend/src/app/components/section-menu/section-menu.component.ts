12f756d04bb88f4099cdf43304545a3b17b447e4
DJCortesz <DJCortes2000@gmail.com>
2022-12-07 15:05:15 -0800

    section html placed into its own component

--- frontend/src/app/components/section-menu/section-menu.component.ts
+++ frontend/src/app/components/section-menu/section-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CourseService } from 'src/app/services/course.service';
+import { CourseDetailComponent } from '../course-detail/course-detail.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-section-menu',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./section-menu.component.css']
 })
 export class SectionMenuComponent {
+
+  constructor(private route: ActivatedRoute, private courseDetail: CourseDetailComponent) { }
+
+  ngOnInit(): void {
+
+  }
+
 
 }
